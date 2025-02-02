5cd1733678a62f38dacb41d8bb5b5f2bbe555f48
Peter Brian Gwadenya <75813303+pgwiz@users.noreply.github.com>
2025-02-02 11:22:46 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
-pip3 install -r requirements.txt
-python3.9 manage.py collectstatic
-#rsync -avz --delete <local-directory>/ <user>@<vps-ip>:<remote-directory>/
-#git pull origin main
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Apply database migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput

910882f143737f34bfb6fce5248cdd2ade74241f
Peter Brian Gwadenya <75813303+pgwiz@users.noreply.github.com>
2025-02-02 11:46:00 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 
 # Exit immediately if a command exits with a non-zero status
 set -e
-apt install postgresql -y
-apt install rust -y
+
+# Install PostgreSQL and Rust
+apt-get update
+apt-get install -y libpq-dev rustc
+
 # Install Python dependencies
 pip install -r requirements.txt
 
